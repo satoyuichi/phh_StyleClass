bd62f342d5144cae83df06fd5de860fbfca88c4d
Yuichi Sato <y_sato@nnn.ed.jp>
2017-06-22 15:56:26 +0900

    List 6-37

--- style_class.js
+++ style_class.js
@@ -1,14 +1,19 @@
 document.addEventListener ('DOMContentLoaded', function () {
   let elem = document.getElementById ('elem');
 
-  // マウスポインターが乗ったタイミングで背景色を変更
-  elem.addEventListener ('mouseover', function() {
-    this.className = 'highlight';
-  }, false);
+  // // マウスポインターが乗ったタイミングで背景色を変更
+  // elem.addEventListener ('mouseover', function() {
+  //   this.className = 'highlight';
+  // }, false);
 
-  // マウスポインターが乗ったタイミングで背景色を戻す
-  elem.addEventListener ('mouseout', function () {
-    this.className = '';
+  // // マウスポインターが乗ったタイミングで背景色を戻す
+  // elem.addEventListener ('mouseout', function () {
+  //   this.className = '';
+  // }, false);
+
+  // クリックしたタイミングで背景色を変更
+  elem.addEventListener ('click', function () {
+    this.className = (this.className === 'highlight' ? '' : 'highlight');
   }, false);
 
 }, false);

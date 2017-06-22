3f5c803fdef4287ab35464a981ba919e67eed303
Yuichi Sato <y_sato@nnn.ed.jp>
2017-06-22 15:23:06 +0900

    Fix

--- style_class.js
+++ style_class.js
@@ -1,2 +1,11 @@
 document.addEventListener ('DOMContentLoaded', function () {
+  let elem = document.getElementById ('elem');
+
+  elem.addEventListener ('mouseover', function() {
+    this.className = 'highlight';
+  }, false);
+
+  elem.addEventListener ('mouseout', function () {
+    this.className = '';
+  }, false);
 }, false);
